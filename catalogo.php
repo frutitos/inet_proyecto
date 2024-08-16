fb2affaf508f9973d45ad62ceedbb79ee6d14bf4
carlos soliz <108693593+CarlosSolizSiles@users.noreply.github.com>
2024-08-16 13:12:25 +0000

    emaquetacion 3 paginas

---
+++ catalogo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Catálogo</title>
+</head>
+<body>
+    <header>
+        <img src="logo.png" alt="Logo">
+        <nav>
+            <ul>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="catalogo.html">Catálogo</a></li>
+                <li><a href="asistencia.html">Asistencia al Cliente</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Catálogo de Productos</h1>
+        <p>Contenido principal de la página de catálogo.</p>
+    </main>
+    <footer>
+        <p>Un pie de página</p>
+    </footer>
+</body>
+</html>
