68535215639820a341358ac1370214fc958334d5
carlos soliz <carlos.soliz.t1vl@gmail.com>
2024-08-15 21:24:53 -0300

    2.0

---
+++ index2.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+          }
+        }
+      }
+    }
+  </script>
+</head>
+<body>
+    <header class="h-16 bg-red-200">
+        <nav>
+            <ul class="flex ">
+                <li><a href=""><img src="" alt="logo"></a></li>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Cátegoria</a></li>
+                <li><a href="">Ayuda</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
